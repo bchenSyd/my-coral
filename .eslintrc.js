2e7ed87318abbed1f4c34519957a1b051793bc5c
Bo Chen <bo.chen@westpac.com.au>
2018-07-04 14:07:07 +1000

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true, // understand require keyword;
+    jest: true
+  },
+  // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parser: "babel-eslint", // for  state = { toggle: false} // Unexpected token = (which is purely a babel syntax)
+  plugins: ["react"],
+  rules: {
+    "react/jsx-filename-extension": "off"
+  }
+};
