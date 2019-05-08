864fb6a8dcaa6e45ce4ea356bdcce4523688fdec
Bo Chen <bo.chen@westpac.com.au>
2019-05-08 16:05:35 +1000

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   // https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
   extends: ["airbnb", "plugin:prettier/recommended"],
-  parser: "babel-eslint", // for  state = { toggle: false} // Unexpected token = (which is purely a babel syntax)
+  parser: "babel-eslint", // for  state = { toggle: false} // Unexpected token = (which is purely a babel syntax); @babel/plugin-proposal-class-properties
   plugins: ["react"],
   rules: {
     "react/jsx-filename-extension": "off"
