c4030df69b78bcdad87290ad4b830c5a864083df
Bo Chen <bo.chen@westpac.com.au>
2019-05-21 14:58:30 +1000

    update

--- src/cards/index.js
+++ src/cards/index.js
@@ -3,12 +3,14 @@ import Tasks from "./tasks";
 import AccountsHoldings from "./accountsHoldings";
 import BorrowingPower from "./borrwoingPower";
 import Upcoming from "./upcoming";
+import Needs from "./needs";
 
 export { Appointments, Tasks, AccountsHoldings, BorrowingPower, Upcoming };
 
 export const cardMap = new Map([
   ["today", Appointments],
   ["todo", Tasks],
+  ["needs", Needs],
   ["accountsHolding", AccountsHoldings],
   ["borrowingPower", BorrowingPower],
   ["upcoming", Upcoming]
---
+++ src/cards/needs.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Needs extends Component {
+  static title = "Needs & Goals";
+  
+  render() {
+    return (
+      <div>
+        content goes here content goes here content goes here content goes here
+        content goes here content goes here content goes here
+      </div>
+    );
+  }
+}
+
+export default Needs;
